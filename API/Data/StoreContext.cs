64f8d3b3ad48d66339eab56a613148be434ac839
uvini917 <uvini917@gmail.com>
2022-01-20 22:46:13 +0530

    End of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+
+
+    }
+
+
+}
+
+
